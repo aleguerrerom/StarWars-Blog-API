187c089fed7d7467d3e1e7181ee83b9be07265ab
Alejandro <Aleguemad94@yahoo.es>
2021-03-30 18:15:35 +0000

    AVANCE

--- src/models.py
+++ src/models.py
@@ -17,3 +17,39 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    gender = db.Column(db.String(250), unique=False, nullable=False)
+    eye = db.Column(db.String(250), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "eye": self.eye,
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    population = db.Column(db.String(250), unique=False, nullable=False)
+    terrain = db.Column(db.String(250), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.planets
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "terrain": self.terrain,
+            # do not serialize the password, its a security breach
+        }
